5bd54e4e452352ca31b0244f63fc747f8c94ac10
taMil-li <tamildeveloper2007@gmail.com>
2025-03-31 23:59:24 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,5 @@
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
